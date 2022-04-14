0252ed5f81dfec9bac23c31cd77161851327cc21
Joseph Lteif <JosephLteif@outlook.com>
2022-04-14 11:32:21 +0300

    Card Element UI fixes made

--- lib/widgets/CardElement.dart
+++ lib/widgets/CardElement.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:smarthomeautomation/providers/AppearanceState.dart';
@@ -6,43 +7,46 @@ class CardElement extends StatelessWidget {
   String title, unit, value, device;
   IconData icon;
   Color color;
-  
-  CardElement({required this.color, required this.icon, required this.title, required this.unit, required this.value, required this.device});
+
+  CardElement(
+      {Key? key, required this.color,
+      required this.icon,
+      required this.title,
+      required this.unit,
+      required this.value,
+      required this.device}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    bool isDarkMode = Provider.of<AppearanceState>(context, listen: false).isDarkMode;
+    bool isDarkMode =
+        Provider.of<AppearanceState>(context, listen: false).isDarkMode;
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
       height: 150,
       width: 150,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
-        gradient: !isDarkMode?LinearGradient(
-          colors: [
-            color,
-            Color.fromARGB(255, 242, 242, 243),
-          ],
-          begin: const FractionalOffset(0.0, 0.0),
-          end: const FractionalOffset(0.0, 2.5),
-          stops: const [0.0, 1.0],
-          tileMode: TileMode.clamp):
-          LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 26, 26),
-            color
-          ],
-          begin: const FractionalOffset(0.0, -.5),
-          end: const FractionalOffset(0.0, 1.0),
-          stops: const [0.0, 1.0],
-          tileMode: TileMode.clamp)
-          ,
+        gradient: !isDarkMode
+            ? LinearGradient(
+                colors: [
+                  color,
+                  const Color.fromARGB(255, 242, 242, 243),
+                ],
+                begin: const FractionalOffset(0.0, 0.0),
+                end: const FractionalOffset(0.0, 2.5),
+                stops: const [0.0, 1.0],
+                tileMode: TileMode.clamp)
+            : LinearGradient(
+                colors: [const Color.fromARGB(255, 26, 26, 26), color],
+                begin: const FractionalOffset(0.0, -.5),
+                end: const FractionalOffset(0.0, 1.0),
+                stops: const [0.0, 1.0],
+                tileMode: TileMode.clamp),
         boxShadow: [
           BoxShadow(
-            color: color.withOpacity(isDarkMode?0.6:0.0),
+            color: color.withOpacity(isDarkMode ? 0.6 : 0.0),
             offset: const Offset(1, 3),
-            blurRadius: 10,
-            // spreadRadius: 3.0,
+            blurRadius: 1,
           ),
         ],
       ),
@@ -53,13 +57,73 @@ class CardElement extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(title, style: const TextStyle(color: Colors.white),),
+              Text(
+                title,
+                style: const TextStyle(color: Colors.white),
+              ),
               Icon(icon, color: Colors.white)
             ],
           ),
-          Spacer(flex: 2,),
-          Text("$value$unit", style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25)),
-          Spacer(flex: 1,),
+          const Spacer(
+            flex: 1,
+          ),
+          Flexible(
+            child: LineChart(LineChartData(
+              titlesData: FlTitlesData(
+                show: false,
+              ),
+              borderData: FlBorderData(
+                  show: false,
+                  border: Border.all(color: const Color(0xff37434d), width: 1)),
+              minX: 0, //lowest value in the x axis
+              maxX: 11, //highest value in the x axis
+              minY: 0, //lowest value in the y axis
+              maxY: 20, //highest value in the y axis
+              lineBarsData: [
+                LineChartBarData(
+                  spots: const [
+                    FlSpot(0, 8),
+                    FlSpot(2.6, 1),
+                    FlSpot(4.9, 20),
+                    FlSpot(6.8, 12),
+                    FlSpot(8, 4),
+                    FlSpot(9.5, 20),
+                    FlSpot(11, 4),
+                  ],
+                  isCurved: true,
+                  gradient: const LinearGradient(
+                    colors: [Colors.white, Colors.white],
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                  ),
+                  barWidth: 5,
+                  isStrokeCapRound: true,
+                  dotData: FlDotData(
+                    show: false,
+                  ),
+                  belowBarData: BarAreaData(
+                    show: false,
+                    gradient: const LinearGradient(
+                      colors: [Colors.transparent, Colors.transparent],
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                    ),
+                  ),
+                ),
+              ],
+            )),
+          ),
+          const Spacer(
+            flex: 2,
+          ),
+          Text("$value $unit",
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25)),
+          const Spacer(
+            flex: 1,
+          ),
           Text(device, style: const TextStyle(color: Colors.white)),
         ],
       ),
