1ed58d26c27d248a11802d4fbbdd534cb841d52a
Joseph Lteif <JosephLteif@outlook.com>
2022-04-15 12:36:51 +0300

    Temp Slider UI Changed

--- lib/views/RoomsPage.dart
+++ lib/views/RoomsPage.dart
@@ -1,7 +1,10 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sleek_circular_slider/sleek_circular_slider.dart';
+import 'package:smarthomeautomation/providers/AppearanceState.dart';
+import 'package:smarthomeautomation/utils/colors.dart';
 import 'package:smarthomeautomation/widgets/CardElement.dart';
 import 'package:smarthomeautomation/widgets/DataInfoBar.dart';
 import 'package:smarthomeautomation/widgets/TopRowBar.dart';
@@ -15,6 +18,7 @@ class RoomsPage extends StatefulWidget {
 
 class _RoomsPageState extends State<RoomsPage> {
   double currentTemp = 0;
+
   List<Color> colors = [
     const Color(0xFF6f5eff),
     const Color(0xFF14c3b5),
@@ -41,6 +45,10 @@ class _RoomsPageState extends State<RoomsPage> {
   ];
   @override
   Widget build(BuildContext context) {
+    bool isDarkMode =
+        Provider.of<AppearanceState>(context, listen: false).isDarkMode;
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
@@ -62,42 +70,92 @@ class _RoomsPageState extends State<RoomsPage> {
               const SizedBox(
                 height: 35,
               ),
-              SleekCircularSlider(
-                // TODO change min and max for temp slider
-                max: 100,
-                min: 0,
-                appearance: CircularSliderAppearance(
-                  size: MediaQuery.of(context).size.width * 0.7,
-                  customWidths: CustomSliderWidths(
-                      progressBarWidth: 40, trackWidth: 70, handlerSize: 22),
-                  customColors: CustomSliderColors(
-                      progressBarColors: [
-                        Colors.red,
-                        Colors.red,
-                        Colors.blue,
-                      ],
-                      trackColor: Colors.white,
-                      dotColor: Colors.white,
-                      hideShadow: false,
-                      shadowColor: Colors.black.withOpacity(0.01)),
-                ),
-                onChangeEnd: (double value) {
-                  // TODO something about temp slider value
-                },
-                innerWidget: (value) => Center(
-                    child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "${value.toStringAsPrecision(3)} °C",
-                      style: const TextStyle(fontSize: 30),
-                    ),
-                    const Text(
-                      "Temp",
-                      style: TextStyle(fontSize: 28),
+              Container(
+                height: width * 0.7,
+                width: width * 0.7,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      blurRadius: 25,
+                      spreadRadius: 5,
+                      offset: const Offset(0, 0),
                     ),
                   ],
-                )),
+                ),
+                child: SleekCircularSlider(
+                  // TODO change min and max for temp slider
+                  max: 100,
+                  min: 0,
+                  appearance: CircularSliderAppearance(
+                    size: width * 0.7,
+                    customWidths: CustomSliderWidths(
+                        progressBarWidth: 40, trackWidth: 70, handlerSize: 22),
+                    customColors: CustomSliderColors(
+                        progressBarColors: [
+                          Colors.red,
+                          Colors.red,
+                          Colors.blue,
+                        ],
+                        trackColor:
+                            isDarkMode ? const Color(0xFF253c56) : Colors.white,
+                        dotColor: Colors.white,
+                        hideShadow: isDarkMode ? true : false,
+                        shadowColor: Colors.black.withOpacity(0.01)),
+                  ),
+                  onChangeEnd: (double value) {
+                    // TODO something about temp slider value
+                  },
+                  innerWidget: (value) => Stack(children: [
+                    Center(
+                      child: Container(
+                        height: height * 0.22,
+                        width: height * 0.22,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                          gradient: isDarkMode
+                              ? LinearGradient(
+                                  colors: [
+                                    const Color(0xFF2b2e46),
+                                    darkColorTheme
+                                  ],
+                                  begin: const FractionalOffset(0.0, 0.0),
+                                  end: const FractionalOffset(0.0, 2.5),
+                                )
+                              : const LinearGradient(
+                                  colors: [Colors.white, Colors.white],
+                                  begin: FractionalOffset(0.0, 0.0),
+                                  end: FractionalOffset(0.0, 2.5),
+                                ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: isDarkMode
+                                  ? darkColorTheme.withOpacity(0.3)
+                                  : lightColorTheme.withOpacity(0.3),
+                              blurRadius: 10,
+                              spreadRadius: 5,
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "${value.toStringAsPrecision(3)} °C",
+                              style: const TextStyle(fontSize: 30),
+                            ),
+                            const Text(
+                              "Temp",
+                              style: TextStyle(fontSize: 28),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ]),
+                ),
               ),
               SizedBox(
                 height: 180,
